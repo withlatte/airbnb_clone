6f7a39c4f22b2d7ce7a4c0579c40ab3ebc1c1a94
jpark <withlatte@naver.com>
2020-03-17 22:06:46 +0900

    completed Lesson #5.3

--- airbnb_clone2/settings.py
+++ airbnb_clone2/settings.py
@@ -44,9 +44,12 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
+    "conversations.apps.ConversationsConfig",
 ]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
