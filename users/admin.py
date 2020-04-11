928c0f5c8f64eab01236a2e48ba6be93a4a51952
jpark <withlatte@naver.com>
2020-04-11 21:41:02 +0900

    completed Lesson #17.4

--- users/admin.py
+++ users/admin.py
@@ -22,6 +22,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
@@ -41,5 +42,5 @@ class CustomUserAdmin(UserAdmin):
         "is_staff",
         "is_superuser",
         "email_verified",
-        "email_secret",
+        "login_method",
     )
--- users/models.py
+++ users/models.py
@@ -37,6 +37,16 @@ class User(AbstractUser):
         (CURRENCY_KRW, "KRW"),
     )
 
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGIN_KAKAO, "Kakao"),
+    )
+
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True,)
     bio = models.TextField(blank=True)
@@ -51,6 +61,10 @@ class User(AbstractUser):
 
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=120, default="", blank=True)
+
+    login_method = models.CharField(
+        choices=LOGIN_CHOICES, max_length=10, blank=True, default=LOGIN_EMAIL
+    )
 
     def verify_email(self):
         if self.email_verified is False:
