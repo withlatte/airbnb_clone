1f2ae168da5f3cb851ddeb095f530555cf3def11
jpark <withlatte@naver.com>
2020-04-19 11:34:00 +0900

    completed Lesson #20.6

--- rooms/models.py
+++ rooms/models.py
@@ -105,3 +105,7 @@ class Room(core_models.TimeStampedModel):
                 all_rating += review.rating_average()
             return round(all_rating / len(all_reviews), 2)
         return 0
+
+    def first_photo(self):
+        (photo,) = self.photos.all()[:1]
+        return photo.file.url
