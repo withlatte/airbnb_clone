c1449067b2371a67e3351124110ee36c8398f6ef
jpark <withlatte@naver.com>
2020-05-05 20:20:12 +0900

    completed Lesson #24.7

--- cal.py
+++ cal.py
@@ -4,9 +4,11 @@ from django.utils import timezone
 
 
 class Day:
-    def __init__(self, number, past):
+    def __init__(self, number, past, month, year):
         self.number = number
         self.past = past
+        self.month = month
+        self.year = year
 
     def __str__(self):
         return str(self.number)
@@ -45,7 +47,12 @@ class Calendar(calendar.Calendar):
             # for day, _ in week: 라고 표현할 수도 있다.
             # _ 는 week_day 값이 무엇이든 사용하지 않을 것이다라는 의미. null 처럼 사용
             for day, week_day in week:
-                new_day = Day(day, day <= today)
+                new_day = Day(
+                    number=day,
+                    past=bool(day <= today),
+                    month=self.month,
+                    year=self.year,
+                )
                 days.append(new_day)
         return days
 
--- reservations/admin.py
+++ reservations/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from . import models
 
 
-# Register your models here.
 @admin.register(models.Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     """ Reservation Admin Definition """
@@ -18,3 +17,10 @@ class ReservationAdmin(admin.ModelAdmin):
     )
 
     list_filter = ("status",)
+
+
+@admin.register(models.BookedDay)
+class BookedDayAdmin(admin.ModelAdmin):
+    """ Booked Day Admin Definition """
+
+    list_display = ("day", "reservation")
