4c974fddf6c65cd99a981355b06169a25f1c1165
jpark <withlatte@naver.com>
2020-05-06 23:36:27 +0900

    completed Lesson #24.12

--- reservations/models.py
+++ reservations/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
+from . import managers
 
 
 class BookedDay(core_models.TimeStampedModel):
@@ -31,7 +32,9 @@ class Reservation(core_models.TimeStampedModel):
         (STATUS_CANCELED, "Canceled"),
     )
 
-    status = models.CharField(max_length=12, choices=STATUS_CHOICES,)
+    status = models.CharField(
+        max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING
+    )
     check_in = models.DateField()
     check_out = models.DateField()
     guest = models.ForeignKey(
@@ -40,6 +43,8 @@ class Reservation(core_models.TimeStampedModel):
     room = models.ForeignKey(
         "rooms.Room", related_name="reservations", on_delete=models.CASCADE
     )
+
+    objects = managers.CustomReservationManager()
 
     def __str__(self):
         return f"{self.room} - {self.check_in}"
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+import datetime
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import View
+from django.http import Http404
+from rooms import models as room_models
+from django.contrib import messages
+from . import models
 
-# Create your views here.
+
+class CreateError(Exception):
+    pass
+
+
+def create(request, room, year, month, day):
+    try:
+        date_obj = datetime.datetime(year=year, month=month, day=day)
+        room_obj = room_models.Room.objects.get(pk=room)
+        models.BookedDay.objects.get(day=date_obj, reservation__room=room_obj)
+        raise CreateError()
+    except (room_models.Room.DoesNotExist, CreateError):
+        messages.error(request, "Can't make reservation on this room")
+        return redirect(reverse("core:home"))
+    except models.BookedDay.DoesNotExist:
+        date_obj = datetime.datetime(year=year, month=month, day=day)
+        room_obj = room_models.Room.objects.get(pk=room)
+        reservation = models.Reservation.objects.create(
+            guest=request.user,
+            room=room_obj,
+            check_in=date_obj,
+            check_out=date_obj + datetime.timedelta(days=1),
+        )
+        return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
+
+
+class ReservationDetailView(View):
+    """ Reservation Detail View Definition """
+
+    def get(self, *args, **kwargs):
+        pk = self.kwargs.get("pk")
+        # models.MYCLASS.objects 를 model manager 라고하며, managers.py 를 통해 오버라이드 가능하다.
+        #  get_object_or_404 method 를 사용하는 것도 get_on_none 과 동일한 기능을 구현한다.
+        reservation = models.Reservation.objects.get_or_none(pk=pk)
+        if not reservation or (
+            reservation.guest != self.request.user
+            and reservation.room.host != self.request.user
+        ):
+            raise Http404()
+        return render(
+            request=self.request,
+            template_name="reservations/detail.html",
+            context={"reservation": reservation},
+        )
+
+
+def edit_reservation(request, pk, verb):
+    reservation = models.Reservation.objects.get_or_none(pk=pk)
+
+    if not reservation or (
+        reservation.guest != request.user and reservation.room.host != request.user
+    ):
+        raise Http404()
+    if verb == "confirmed":
+        reservation.status = models.Reservation.STATUS_CONFIRMED
+    elif verb == "canceled":
+        reservation.status = models.Reservation.STATUS_CANCELED
+
+    reservation.save()
+    messages.success(request, f"Your reservation has been {verb}")
+
+    return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
