d2b26342e0026d8087267c0ca3d419ac978206cd
jpark <withlatte@naver.com>
2020-05-10 20:46:09 +0900

    completed Lesson #26.2

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""airbnb_clone2 URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import dotenv
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airbnb_clone2.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
