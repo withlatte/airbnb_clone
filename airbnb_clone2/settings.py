a3e60d9e94f8dcc2ce99143bccbb3e3fa965e420
jpark <withlatte@naver.com>
2020-05-13 23:11:51 +0900

    completed Lesson #26.2

--- airbnb_clone2/settings.py
+++ airbnb_clone2/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com"]
+ALLOWED_HOSTS = [".elasticbeanstalk.com", "localhost"]
 
 # Application definition
 
